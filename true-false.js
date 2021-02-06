780cff299e0cca6ff5a4d815c42e262bb40937f3
sakhawathossensadi <newtonss919@gmail.com>
2021-02-06 20:20:53 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//False value :
+// 0
+// "" (empty string)
+// undefined
+//null
+// NaN (Not a Number)
+//false
+//Truth value :
+//"0"
+//true
+
+const age = 10;
+if(age>5)
+{
+    console.log("Condition is true");
+}
+else
+{
+    console.log("Condition is false");
+}
